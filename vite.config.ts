6700dc9d7fff55b11a137e5f490f23693eed6788
deeharvey06 <cliffharvey06@gmail.com>
2026-07-20 10:31:03 -0700

    types/node

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://deeharvey06.github.io/deploycraft.tech/', // Replace with your desired base path
+  base: '/deploycraft.tech', // Replace with your desired base path
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
